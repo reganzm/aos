64f136ef36b2e810110a84142b671de59c90607e
626692024@qq.com <626692024@qq.com>
2024-08-01 18:24:56 +0800

    [add]vga and print and test

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,87 @@
 // 禁用标准库
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+mod serial;
+mod vga_buffer;
 static HELLO: &[u8] = b"Hello rust!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xa;
-        }
-    }
+    //let vga_buffer = 0xb8000 as *mut u8;
+    //for (i, &byte) in HELLO.iter().enumerate() {
+    //    unsafe {
+    //        *vga_buffer.offset(i as isize * 2) = byte;
+    //        *vga_buffer.offset(i as isize * 2 + 1) = 0xd9;
+    //    }
+    //}
+    //vga_buffer::print_something();
+    //use core::fmt::Write;
+    //vga_buffer::WRITER.lock().write_str("Hello world").unwrap();
+    //write!(
+    //    vga_buffer::WRITER.lock(),
+    //    "some numbers:{} {}",
+    //    234,
+    //    1. / 3.
+    //)
+    // .unwrap();
 
+    println!("hello world! {}", "regan");
+    //panic!("SOME panic message");
+    #[cfg(test)]
+    test_main();
     loop {}
 }
 
 // 自定义panic处理函数
 use core::panic::PanicInfo;
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error:{}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_println!("trivial assertion....");
+    assert_eq!(1, 2);
+    serial_println!("ok");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
